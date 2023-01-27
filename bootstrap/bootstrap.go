d57ca8f2816e87dbf44d2b54a4da01e1b6d65a00
Damodar Bastakoti <damodar.bastakoti9@gmail.com>
2023-01-27 14:58:43 +0545

    feat:bootstrap

--- bootstrap/bootstrap.go
+++ bootstrap/bootstrap.go
@@ -1,6 +1,7 @@
 package bootstrap
 
 import (
+	"context"
 	"letschat/api/controllers"
 	middlewares "letschat/api/middlewares"
 	"letschat/api/repository"
@@ -16,17 +17,51 @@ var Module = fx.Options(
 	infrastructure.Module,
 	routes.Module,
 	middlewares.Module,
+
 	services.Module,
 	repository.Module,
+
 	fx.Invoke(bootstrap),
 )
 
 func bootstrap(
+	lifecycle fx.Lifecycle,
 	handler infrastructure.Router,
 	routes routes.Routes,
+	database infrastructure.Database,
+	logger infrastructure.Logger,
 	env infrastructure.Env,
 ) {
+	appStop := func(context.Context) error {
+		logger.Zap.Info("Stopping Application")
+
+		// conn.Close()
+		return nil
+	}
 	routes.Setup()
 	handler.Gin.Run(":" + env.ServerPort)
+
+	lifecycle.Append(fx.Hook{
+		OnStart: func(context.Context) error {
+			logger.Zap.Info("Starting Application")
+			logger.Zap.Info("------------------------")
+			logger.Zap.Info("------  Chat Application  📺  ------")
+			logger.Zap.Info("------------------------")
+
+			go func() {
+				//middlewares.Setup()
+				routes.Setup()
+				logger.Zap.Info("🌱 Seeding data...")
+				//seeds.Run()
+				if env.ServerPort == "" {
+					handler.Gin.Run()
+				} else {
+					handler.Gin.Run(":" + env.ServerPort)
+				}
+			}()
+			return nil
+		},
+		OnStop: appStop,
+	})
 
 }
