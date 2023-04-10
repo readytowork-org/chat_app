e56e9f295e08109cd21096b5882859e33d17468c
Damodar Bastakoti <damodar.bastakoti9@gmail.com>
2023-04-10 10:09:04 +0545

    feat:delete firebase auth / firebase service / firebase crud / firebase middleware

--- api/middlewares/middleware.go
+++ api/middlewares/middleware.go
@@ -3,7 +3,5 @@ package middlewares
 import "go.uber.org/fx"
 
 var Module = fx.Options(
-	// fx.Provide(NewMiddewares),
-	fx.Provide(NewFirebaseAuth),
 	fx.Provide(NewJWTAuthMiddleWare),
 )
--- api/services/firebase.go
+++ api/services/firebase.go
@@ -3,85 +3,45 @@ package services
 import (
 	"context"
 	"letschat/infrastructure"
-	"letschat/models"
+	"log"
 
-	"firebase.google.com/go/v4/auth"
+	"firebase.google.com/go/messaging"
 )
 
 type FirebaseService struct {
-	fbAuth *auth.Client
+	fb     infrastructure.FirebaseApp
 	logger infrastructure.Logger
 }
 
 func NewFirebaseService(
-	fbAuth *auth.Client,
+	fb infrastructure.FirebaseApp,
 	logger infrastructure.Logger,
 ) FirebaseService {
 	return FirebaseService{
-		fbAuth: fbAuth,
+		fb:     fb,
 		logger: logger,
 	}
 }
 
-func (fb *FirebaseService) VerifyToken(idToken string) (*auth.Token, error) {
-	token, err := fb.fbAuth.VerifyIDToken(context.Background(), idToken)
-	return token, err
-}
+func (fs FirebaseService) PushNotification(body string, title string, deviceId string) error {
 
-func (fb *FirebaseService) CreateCustomToken(uid string) (string, error) {
-	token, err := fb.fbAuth.CustomToken(context.Background(), uid)
-	return token, err
-}
-
-func (fb *FirebaseService) GetUser(uid string) (*auth.UserRecord, error) {
-	user, err := fb.fbAuth.GetUser(context.Background(), uid)
-	return user, err
-}
-
-func (fb *FirebaseService) CreateUser(newUser models.FirebaseAuthUser) (string, error) {
-	params := (&auth.UserToCreate{}).
-		Email(newUser.Email).
-		Password(newUser.Password).
-		DisplayName(newUser.DisplayName).
-		Disabled(false)
-
-	u, err := fb.fbAuth.CreateUser(context.Background(), params)
+	client, err := fs.fb.FA.Messaging(context.Background())
 	if err != nil {
-		return "", err
+		log.Fatalf("error initializing Messaging client: %v\n", err)
+		return err
 	}
-	claims := map[string]interface{}{
-		"role":   newUser.Role,
-		"fb_uid": u.UID,
-		"id":     newUser.UserId,
+	message := &messaging.Message{
+		Notification: &messaging.Notification{
+			Title: title,
+			Body:  body,
+		},
+		Token: deviceId,
 	}
-	err = fb.fbAuth.SetCustomUserClaims(context.Background(), u.UID, claims)
+	response, err := client.Send(context.Background(), message)
 	if err != nil {
-		return "Internal Server Error", err
+		log.Fatalf("error sending message: %v\n", err)
+		return err
 	}
-	tokens, err := fb.CreateCustomToken(u.UID)
-	if err != nil {
-		return "Error Getting tokens", nil
-	}
-	return tokens, err
-}
-
-func (fb *FirebaseService) GetUserByEmail(email string) string {
-	user, _ := fb.fbAuth.GetUserByEmail(context.Background(), email)
-	if user != nil {
-		return user.UID
-	}
-	return ""
-}
-
-func (fb *FirebaseService) LoginUser(user models.FirebaseAuthUser) (string, error) {
-	uid := fb.GetUserByEmail(user.Email)
-	fb.logger.Zap.Info("this is uid ", uid)
-	if len(uid) == 0 {
-		return "Invalid Email", nil
-	}
-	tokens, err := fb.CreateCustomToken(uid)
-	if err != nil {
-		return "Error Getting tokens", nil
-	}
-	return tokens, err
+	log.Printf("message sent successfully: %v\n", response)
+	return nil
 }
